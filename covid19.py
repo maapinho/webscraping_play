6fc3808f1178a685410f5352edb67e691c588aec
Mario <maapinho@hotmail.com>
2020-04-13 03:26:22 +0200

    covid19 Add JSON and datetime

--- covid19.py
+++ covid19.py
@@ -1,5 +1,14 @@
 from gazpacho import get,Soup
 from pprint import pprint
+import json
+from datetime import datetime
+
+#current date time object
+dt=datetime.now().replace(microsecond=0)
+
+
+# Datetim in ISO 8601
+# datetime.datetime.now().replace(microsecond=0).isoformat()
 
 URL = 'http://www.covid19.gov.ao'
 
@@ -12,10 +21,15 @@ labels=soup.find('span',{'class':'f-size2'})
 
 for i in range(len(numbers)):
     numbers[i]=numbers[i].text
-
-for i in range(len(labels)):
     labels[i]=labels[i].text
 
 for i in range(len(numbers)):
     print(labels[i],':',numbers[i])
+
+# data to JSON
+json_data={}
+for i in range(len(numbers)):
+    json_data[labels[i]]=int(numbers[i])
+
+print(json_data)
 
