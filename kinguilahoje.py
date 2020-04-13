eb05e75e3870956be0a84eb2e3e30693896c8e78
Mario <maapinho@hotmail.com>
2020-04-13 03:26:36 +0200

    kinguilahoje Add JSON and datetime

--- kinguilahoje.py
+++ kinguilahoje.py
@@ -1,5 +1,15 @@
 from gazpacho import get,Soup
 from pprint import pprint
+import json
+from datetime import datetime
+
+#current date time object
+dt=datetime.now().replace(microsecond=0)
+print(dt)
+print(dt.isoformat())
+
+# Datetim in ISO 8601
+# datetime.datetime.now().replace(microsecond=0).isoformat()
 
 URL='http://www.kinguilahoje.com/'
 
@@ -14,3 +24,14 @@ euro=quotations[1].text
 #pprint(quotations)
 print('dolar:',dolar)
 print('euro:',euro)
+
+dolar_integer=int(dolar.split()[1])
+euro_integer=int(euro.split()[1])
+
+# data to JSON
+json_data={'Dolar':dolar_integer,'Euro':euro_integer}
+
+#print(json_data)
+
+#output JSON data as a string
+print(json.dumps(json_data))
